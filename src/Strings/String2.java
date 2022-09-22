40db8199f5fc180bac761f6b8987a69aca8e5f0d
Himanshu <himanshu.singh_iot18@gla.ac.in>
2022-09-22 22:09:48 +0530

    git testing

--- src/Strings/String2.java
+++ src/Strings/String2.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class String2 {
 //this is second program
-    public static String sortSentence(String s) {
+    //this is comment for git
 
+    public static String sortSentence(String s) {
+     return "";
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
